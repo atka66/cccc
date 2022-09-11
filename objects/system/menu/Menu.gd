9495a4e291c5365124926056843363393e114185
atka66 <atkavagyok@gmail.com>
2022-09-11 16:24:57 +0200

    quit prompt

--- objects/system/menu/Menu.gd
+++ objects/system/menu/Menu.gd
@@ -1,16 +1,25 @@
 extends Node2D
 
 func _ready():
+	$QuitPrompt.hide()
+	$AppearAudio.stream = Res.AudioPageflip[randi() % len(Res.AudioPageflip)]
 	$AppearAudio.play()
 
 func _input(event):
-	if event.is_action_pressed("quit"):
-		Global.saveGame()
-		get_tree().quit()
-	if event.is_action_pressed("reset"):
-		Global.resetGame()
-	if event.is_action_pressed("ui_cancel"):
-		close()
+	if $QuitPrompt.visible:
+		if event.is_action_pressed("quit"):
+			Global.saveGame()
+			get_tree().quit()
+		if event.is_action_pressed("ui_cancel"):
+			$QuitPrompt.hide()
+	else:
+		if event.is_action_pressed("quit"):
+			$QuitPrompt.show()
+			$QuitPrompt/Anim.play("appear")
+		if event.is_action_pressed("reset"):
+			Global.resetGame()
+		if event.is_action_pressed("ui_cancel"):
+			close()
 
 func close():
 	queue_free()
