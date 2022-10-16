619e7d7bba63b1ba9aea07add737ff91a47a2bd5
atka66 <atkavagyok@gmail.com>
2022-10-16 17:14:53 +0200

    spawn animation

--- objects/player/Spawning.gd
+++ objects/player/Spawning.gd
@@ -4,3 +4,8 @@ export var deathCnt : int = 0
 
 func _ready():
 	$DeathCntLabel.set_text(str(deathCnt))
+	$AnimatedSprite.play()
+
+
+func _on_AnimatedSprite_animation_finished():
+	$AnimatedSprite.hide()
