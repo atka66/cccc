6eaaaeec6b772473a242fc75a2048571f5ef63a6
atka66 <atkavagyok@gmail.com>
2022-10-16 18:41:33 +0200

    desert

---
+++ objects/backdrop/BdDesert.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$BgDesert.margin_left = -(randi() % 400)
+	# TODO plains music
+	#get_node('/root/Music').play(Res.AudioMusicChapterRainy)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
