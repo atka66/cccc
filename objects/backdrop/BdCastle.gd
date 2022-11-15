6ede80b9345b1cf7d6d7d504165e3a113ba9f99d
atka66 <atkavagyok@gmail.com>
2022-11-15 13:08:00 +0100

    map41

--- objects/backdrop/BdCastle.gd
+++ objects/backdrop/BdCastle.gd
@@ -3,8 +3,9 @@ extends Node2D
 const thunderMaxDelay : int = 30
 
 func _ready():
+	$BgCastle.margin_left = -(randi() % 400)
 	# todo castle music
-	get_node('/root/Music').play(Res.AudioMusicChapterRainy)
+	# get_node('/root/Music').play(Res.AudioMusicChapterRainy)
 	$ThunderTimer.start(randf() * thunderMaxDelay)
 
 func _on_ThunderTimer_timeout():
--- objects/mapelement/TogglingTile.gd
+++ objects/mapelement/TogglingTile.gd
@@ -6,13 +6,13 @@ export(float) var duration = 2.0
 export(bool) var toggled = true
 
 func _ready():
+	toggle()
 	$Timer.start(delay)
 
 
 func _on_Timer_timeout():
-	match (toggled):
-		true:
-			$Anim.play("hide")
-		false:
-			$Anim.play("appear")
+	toggle()
 	$Timer.start(duration)
+
+func toggle():
+	$Anim.play("hide" if toggled else "appear")
