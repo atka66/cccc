007075638fcf4de90793543f07ac405f142dec3f
atka66 <atkavagyok@gmail.com>
2022-11-16 16:15:29 +0100

    map51-52

---
+++ objects/backdrop/BdDungeon.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# todo dungeon music
+	# get_node('/root/Music').play(Res.AudioMusicChapterRainy)
+	pass
