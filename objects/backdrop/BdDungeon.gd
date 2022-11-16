1562c7372d31e155374a57a5e6e74173316f2a1c
atka66 <atkavagyok@gmail.com>
2022-11-16 17:57:21 +0100

    cave music

--- objects/backdrop/BdDungeon.gd
+++ objects/backdrop/BdDungeon.gd
@@ -3,5 +3,5 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# todo dungeon music
-	# get_node('/root/Music').play(Res.AudioMusicChapterRainy)
+	get_node('/root/Music').play(Res.AudioMusicChapterCave)
 	pass
