77bc9bd7936d8085edce8f27cf35221dede51017
atka66 <atkavagyok@gmail.com>
2022-11-20 21:36:08 +0100

    more crystals

--- objects/backdrop/BdCave.gd
+++ objects/backdrop/BdCave.gd
@@ -2,5 +2,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$BgCave.margin_left = -(randi() % 400)
+	var offset = -(randi() % 400)
+	$BgCave.margin_left = offset
+	$BgCrystals1.margin_left = offset
+	$BgCrystals2.margin_left = offset
+	$Anim.seek(randf() * $Anim.current_animation_length)
 	get_node('/root/Music').play(Res.AudioMusicChapterCave)
