c2ad2ca911c8db6c524f209d55109070dd4fe7c5
atka66 <atkavagyok@gmail.com>
2022-11-21 18:23:37 +0100

    fix

--- objects/backdrop/BdSnow.gd
+++ objects/backdrop/BdSnow.gd
@@ -8,5 +8,5 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$BgDesert.margin_left = -(randi() % 400)
-	get_node('/root/Music').mute()
+	$BgSnow.margin_left = -(randi() % 400)
+	get_node('/root/Music').play(Res.AudioMusicChapterSnow)
