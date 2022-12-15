6d0b49c831395e6914d74bedb79d5a54619d35d6
atka66 <atkavagyok@gmail.com>
2022-12-15 13:00:10 +0100

    desert wind

--- objects/mapelement/Firefly.gd
+++ objects/mapelement/Firefly.gd
@@ -4,13 +4,16 @@ export(Vector2) var origin = Vector2.ZERO
 
 const P_GOTO_ORIGIN = 10
 
-func _process(delta):
-	if (randi() % P_GOTO_ORIGIN == 0):
-		global_position = global_position.move_toward(origin, 1)
-	else:
-		position += Vector2((randi() % 3) - 1, (randi() % 3) - 1)
 
 
 func _on_FlickerTimer_timeout():
 	$Light2D.energy = randf() + 0.7
 	$FlickerTimer.start(0.1)
+
+
+func _on_MoveTimer_timeout():
+	if (randi() % P_GOTO_ORIGIN == 0):
+		global_position = global_position.move_toward(origin, 1)
+	else:
+		position += Vector2((randi() % 3) - 1, (randi() % 3) - 1)
+	$MoveTimer.start(0.07)
