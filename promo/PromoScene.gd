d2ad7384e5bcec8cecda3c7f2a3564d104641afb
atka66 <atkavagyok@gmail.com>
2024-08-24 19:12:33 +0200

    promo stuff

---
+++ promo/PromoScene.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	yield(get_tree().create_timer(10.0), "timeout")
+	$AnimationPlayer.play("appear")
